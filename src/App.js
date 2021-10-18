725651538cdcddb831d1830797bddc490d4fb075
Ravi Kumar <ravikmar601@gmail.com>
2021-10-19 01:13:18 +0530

    fixed the styling issue of the clock

--- src/App.js
+++ src/App.js
@@ -4,11 +4,10 @@ export default class AppDiv extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
-    this.setAttribute('class', 'App');
     const linkStyle = document.createElement('link');
     linkStyle.setAttribute('rel', 'stylesheet');
     linkStyle.setAttribute('href', './App.css');
-    this.shadowRoot.innerHTML = `<clock-div class='Clock'></clock-div>`;
+    this.shadowRoot.innerHTML = `<clock-div></clock-div>`;
     // this.appendChild(document.createElement("clock-div"));
   }
 }
