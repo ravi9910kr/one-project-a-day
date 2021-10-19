e1a484a0a0f231a1df789b997952e29290134afc
Ravi Kumar <ravikmar601@gmail.com>
2021-10-19 13:33:45 +0530

    forgot to commit this

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ export default class AppDiv extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
+    // importing css
     const linkStyle = document.createElement('link');
-    linkStyle.setAttribute('rel', 'stylesheet');
-    linkStyle.setAttribute('href', './App.css');
-    this.shadowRoot.innerHTML = `<clock-div></clock-div>`;
-    // this.appendChild(document.createElement("clock-div"));
+    linkStyle.setAttribute('rel', "stylesheet");
+    linkStyle.setAttribute('href', "../src/App.css");
+    this.shadowRoot.append(linkStyle); 
+
+    // creating elements
+    const clockDiv = document.createElement("clock-div");
+
+    // adding the children to the dom
+    this.shadowRoot.appendChild(clockDiv);
   }
 }
 
